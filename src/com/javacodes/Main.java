bafd5685e65a221042efcaf876ef29c7c18621df
Arnav Rajput <arnavvrajput@gmail.com>
2025-05-13 23:37:34 +0530

    third commit

--- src/com/javacodes/Main.java
+++ src/com/javacodes/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Put in your employee id: ");
         Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+        int empId = input.nextInt();
+        System.out.println("Your jail roll number is: " + empId);
     }
 }
