d24390057497658017821d3662327c28568174db
Arnav Rajput <arnavvrajput@gmail.com>
2025-05-15 20:28:12 +0530

    more data types

--- src/com/javacodes/Primitives.java
+++ src/com/javacodes/Primitives.java
@@ -5,14 +5,18 @@ public class Primitives {
         int intNumber = 23;
         float floatNumber = 2335.235f;
         double largeDecimalNumber = 23.534593;
-        char letter = 'f';
+        char letter = 'c';
         long largeIntegerNumber = 2359853L;
+        short a = 27;
+        byte b = 123;
 
         System.out.println(intNumber);
         System.out.println(floatNumber);
         System.out.println(largeDecimalNumber);
         System.out.println(largeIntegerNumber);
         System.out.println(letter);
+        System.out.println(a);
+        System.out.println(b);
 
      }
 }
