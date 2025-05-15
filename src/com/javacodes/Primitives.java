93538e449bf842d5a4ce0900c865e0bd53f41453
Arnav Rajput <arnavvrajput@gmail.com>
2025-05-15 19:37:32 +0530

    primite data types

---
+++ src/com/javacodes/Primitives.java
@@ -0,0 +1,18 @@
+package com.javacodes;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int intNumber = 23;
+        float floatNumber = 2335.235f;
+        double largeDecimalNumber = 23.534593;
+        char letter = 'f';
+        long largeIntegerNumber = 2359853L;
+
+        System.out.println(intNumber);
+        System.out.println(floatNumber);
+        System.out.println(largeDecimalNumber);
+        System.out.println(largeIntegerNumber);
+        System.out.println(letter);
+
+     }
+}
